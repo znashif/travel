33fb3ad4c858f7a91fabf1542ac545e15aae8a71
zuhairnashif <zuhairnashif86@gmail.com>
2023-12-04 00:08:53 +0700

    complite component parts/footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends React.Component {
   constructor(props){
@@ -17,6 +19,8 @@ export default class LandingPage extends React.Component {
         <Hero data={landingPage.hero}/>
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial}/>
+        <Footer />
       </>
     );
   }
