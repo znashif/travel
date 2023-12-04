d6cefc68f60c4db3a09704801d443a9e4fb34aa3
zuhairnashif <zuhairnashif86@gmail.com>
2023-12-04 08:07:41 +0700

    ending react-reveal transition

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from "react-reveal/Fade";
 
 import ImageHero from 'assets/images/img-hero.jpg';
 import ImageHero_ from 'assets/images/img-hero-frame.jpg';
@@ -18,7 +19,8 @@ export default function Hero(props) {
     }
 
   return (
-    <section className="container pt-4">
+    <Fade bottom>
+        <section className="container pt-4">
         <div className="row align-items-center">
             <div className="col-auto pr-5" style={{width: 530}}>
                 <h1 className="font-weight-bold line-height-1 mb-3">
@@ -63,5 +65,7 @@ export default function Hero(props) {
             </div>
         </div>
     </section>
+    </Fade>
+    
   )
 }
